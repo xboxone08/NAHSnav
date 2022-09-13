dbc5038bad70caeea4e8a9e3179e4abe481eec75
Xbox One <sondad2008@gmail.com>
2022-09-12 20:53:16 -0400

    Old stuff, new stuff
    - Fixed and added to the route function.
    - Added website skeleton

--- route.py
+++ route.py
@@ -4,6 +4,13 @@ third_floor = tuple()
 
 
 def route(dest_room_number: int, src_room_number: int):
+    # We will need it in string form
+    dest_room_number = str(dest_room_number)
     # Check if they are on the same floor
-    if str(dest_room_number)[0] == str(src_room_number)[0]:
-        
+    if dest_room_number[0] == src_room_number[0]:
+        return (dest_room_number),
+    else:
+        if dest_room_number[2:] < 60:
+            return "wws", dest_room_number
+        else:
+            return "ews", dest_room_number
